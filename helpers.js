a6435b0a3796e676262ed074361ea890f6657347
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-12-03 08:48:05 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *stefanie*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *stefanie*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *stefanie*:  ```" + msg + "```"
 }
 
 
